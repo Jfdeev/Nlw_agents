11781e06966c1ac00ac605c4567efc740ad31f14
Jfdeev <jfrsoares161205@gmail.com>
2025-07-18 11:28:39 -0300

    chore: first table generation

---
+++ src/db/migrations/0000_short_yellow_claw.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "rooms" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"description" text NOT NULL,
+	"desciption" text,
+	"create_at" timestamp DEFAULT now() NOT NULL
+);
