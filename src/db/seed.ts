a34aa159087eabe37d616cf4f5d43c85eafc8490
Jfdeev <jfrsoares161205@gmail.com>
2025-07-22 10:32:28 -0300

    feat: populate db

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,10 +1,22 @@
 import { reset , seed  } from 'drizzle-seed'
 import { db, sql } from './connection.ts';
 import { schema } from './schema/index.ts';
+import { rooms } from './schema/rooms.ts';
+import { desc } from 'drizzle-orm';
 
-await reset(db, {schema});
+await reset(db, schema);
 
-await seed(db, {schema})
+await seed(db, schema).refine(f => {
+    return {
+        rooms: {
+            count: 20,
+            columns: {
+                name: f.companyName(),
+                description: f.loremIpsum()
+            }
+        }
+    }
+})
 
 await sql.end();
 
