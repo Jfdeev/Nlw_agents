c87aaf9cf2f60f0eeb9da3e375b18e9fe8a7d94b
Jfdeev <jfrsoares161205@gmail.com>
2025-07-22 10:38:59 -0300

    feat: create a get route rooms

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 import {fastifyCors} from '@fastify/cors'
 import { sql } from './db/connection.ts'
 import { env } from './env.ts';
+import { getRooms } from './routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -21,6 +22,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () =>{
     return 'OK'
 })
+
+app.register(getRooms)
 
 app.listen({port: env.PORT }).then(() => {
     console.log(`Server is running on http://localhost:${env.PORT}`)
