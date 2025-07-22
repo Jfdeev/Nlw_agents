df8aaf2cc3cc25c734926e0e1d98766749294576
Jfdeev <jfrsoares161205@gmail.com>
2025-07-22 17:29:03 -0300

    feat: implements react router dom

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,8 +1,17 @@
+import { Button } from "./components/ui/button";
+import { Routes, Route } from "react-router-dom";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
 
 
 export function App() {
 
-  return <div className="">Hello World</div>
+  return (
+    <Routes>
+      <Route element={ <CreateRoom />} index/>
+      <Route path="/room" element={<Room />}/>
+    </Routes>
+  )
 }
 
 
---
+++ website/src/pages/create-room.tsx
@@ -0,0 +1,3 @@
+export function CreateRoom() {
+    return <div className="">Create Room</div>
+}
