bb5020ee97049c2fd7d961a70ba091789c107ede
Jfdeev <jfrsoares161205@gmail.com>
2025-07-22 10:45:12 -0300

    feat: implement select to select columns

--- src/routes/get-rooms.ts
+++ src/routes/get-rooms.ts
@@ -1,10 +1,19 @@
 import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { db } from '../db/connection.ts'
 import { schema } from '../db/schema/index.ts'
+import { id } from 'zod/v4/locales'
 
 export const getRooms: FastifyPluginAsyncZod = async (app) => {
     app.get('/rooms', async () => {
-        const results = await db.select().from(schema.rooms).orderBy(schema.rooms.createAt)
+        const results = await db
+        .select({
+            id: schema.rooms.id,
+            name: schema.rooms.name,
+            description: schema.rooms.description,
+            createAt: schema.rooms.createAt,
+        })
+        .from(schema.rooms)
+        .orderBy(schema.rooms.createAt)
 
         return results
     })
