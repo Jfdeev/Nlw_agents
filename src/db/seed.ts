794c9013c84a6ea460904c61c588ac6cd779dd6e
Jfdeev <jfrsoares161205@gmail.com>
2025-07-21 22:51:53 -0300

    feat: add a sql end

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,9 +1,11 @@
 import { reset , seed  } from 'drizzle-seed'
-import { db } from './connection.ts';
+import { db, sql } from './connection.ts';
 import { schema } from './schema/index.ts';
 
 await reset(db, {schema});
 
 await seed(db, {schema})
+
+await sql.end();
 
 console.log('Database seeded successfully');
