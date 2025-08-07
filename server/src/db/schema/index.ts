0122d71d421ae52f588d066f11f0bf27a058afc2
Jfdeev <jfrsoares161205@gmail.com>
2025-08-07 10:23:39 -0300

    feat: create a questions schema

--- server/src/db/schema/index.ts
+++ server/src/db/schema/index.ts
@@ -1,6 +1,8 @@
 //Barrel File (arquivo que exporta tudo que tem dentro)
+import { questions } from './questions.ts';
 import { rooms } from './rooms.ts';
 
 export const schema = {
     rooms,
+    questions
 }
---
+++ server/src/db/schema/questions.ts
@@ -0,0 +1,16 @@
+import { 
+    pgTable,
+    text,
+    timestamp,
+    uuid
+ } from "drizzle-orm/pg-core";
+import { id } from "zod/v4/locales";
+import { rooms } from "./rooms.ts";
+
+ export const questions = pgTable("questions", {
+    id: uuid().primaryKey().defaultRandom(),
+    roomId: uuid().notNull().references(() => rooms.id),
+    question: text().notNull(),
+    answer: text(),
+    createdAt: timestamp().defaultNow().notNull(),
+ })
