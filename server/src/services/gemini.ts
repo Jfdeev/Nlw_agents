7f2d2e6ee6eac9e692da62dbdeb9434ddc7814be
Jfdeev <jfrsoares161205@gmail.com>
2025-08-26 08:53:35 -0300

    feat: implements a logic to trancribe audio

--- server/src/services/gemini.ts
+++ server/src/services/gemini.ts
@@ -7,6 +7,23 @@ const gemini = new GoogleGenAI({
 
 const model = 'gemini-2.5-flash'
 
-export async function transcribeAudio() {
-    gemini.files
+export async function transcribeAudio(audioBase64: string, mimeType: string) {
+    const response = await gemini.models.generateContent({
+        model,
+        contents: {
+            text: 'Transcreva para mim o audio, sem tirar nada e nem gerar coisas a mais, apenas transcreva de forma clara e objetiva. Na lingua Portugues Brasil.',
+            inlineData: {
+                mimeType,
+                data: audioBase64,
+            }
+        },
+        
+    })
+
+    if(!response.text) {
+        throw new Error('Erro ao transcrever o áudio')
+    }
+
+
+    return response.text
 }
