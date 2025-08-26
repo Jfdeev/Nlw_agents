fdfaaecb2114591c50fb6230ccac38bff61de8bc
Jfdeev <jfrsoares161205@gmail.com>
2025-08-26 09:04:24 -0300

    feat: return audio transcription to user

--- server/src/routes/upload-audio.ts
+++ server/src/routes/upload-audio.ts
@@ -1,5 +1,6 @@
 import { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { z } from 'zod/v4'
+import { transcribeAudio } from '../services/gemini.ts'
 import { db } from '../db/connection.ts'
 import { schema } from '../db/schema/index.ts'
 
@@ -20,9 +21,20 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
                 return reply.status(400).send({ error: 'No file uploaded' });
             }
 
-            // 1. Transcrever o audio usando gemini
+            const audioBuffer = await audio.toBuffer();
+            const audioBase64 = audioBuffer.toString('base64');
+            const mimeType = audio.mimetype;
+
+            const transcription = await transcribeAudio(audioBase64, mimeType);
+
+            if (!transcription) {
+                return reply.status(500).send({ error: 'Failed to transcribe audio' });
+            }
+
+            return { transcription }
+
             // 2. Gerar vetor semantico (embeddings)
             // 3. Armazenar os vetores no banco de dados
         }
     )
-}
+} 
