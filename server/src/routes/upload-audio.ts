74408347a6eae439861e08c670b79a9089c83edf
Jfdeev <jfrsoares161205@gmail.com>
2025-08-30 13:53:34 -0300

    feat: create a migration to use a db to store transcriptions

--- server/src/routes/upload-audio.ts
+++ server/src/routes/upload-audio.ts
@@ -3,55 +3,61 @@ import { z } from 'zod/v4'
 import { generateEmbeddings, transcribeAudio } from '../services/gemini.ts'
 import { db } from '../db/connection.ts'
 import { schema } from '../db/schema/index.ts'
+import { sql } from 'drizzle-orm'
+import { error } from 'console'
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
-    app.post('/rooms/:roomId/audio', {
-        schema: {
-            params: z.object({
-                roomId: z.string(),
-            }),
-        },
+  app.post('/rooms/:roomId/audio', {
+    schema: {
+      params: z.object({
+        roomId: z.string(),
+      }),
     },
-        async (request, reply) => {
-            const { roomId } = request.params
-
-            const audio = await request.file();
-
-            if (!audio) {
-                return reply.status(400).send({ error: 'No file uploaded' });
-            }
-
-            const audioBuffer = await audio.toBuffer();
-            const audioBase64 = audioBuffer.toString('base64');
-            const mimeType = audio.mimetype;
-
-            const transcription = await transcribeAudio(audioBase64, mimeType);
-            const embeddings = await generateEmbeddings(transcription);
-
-            if (!transcription) {
-                return reply.status(500).send({ error: 'Failed to transcribe audio' });
-            }
-
-            if (!embeddings) {
-                return reply.status(500).send({ error: 'Failed to generate embeddings' });
-            }
-
-            const result = await db.insert(schema.audioChunks).values({
-                roomId,
-                transcription,
-                embeddings
-            }).returning();
-
-            const chunk = result[0];
-
-            if (!chunk) {
-                return reply.status(500).send({ error: 'Failed to save audio chunk' });
-            }
-
-            return reply.status(201).send({ chunkId: chunk.id });
-
-            // 2. Gerar vetor semantico (embeddings)
-            // 3. Armazenar os vetores no banco de dados
+  },
+    async (request, reply) => {
+      try {
+        
+        const { roomId } = request.params
+  
+        const audio = await request.file();
+  
+        if (!audio) {
+          return reply.status(400).send({ error: 'No file uploaded' });
         }
-    )
+  
+        const audioBuffer = await audio.toBuffer();
+        const audioBase64 = audioBuffer.toString('base64');
+        const mimeType = audio.mimetype;
+  
+        const transcription = await transcribeAudio(audioBase64, mimeType);
+        const embeddings = await generateEmbeddings(transcription);
+  
+        if (!transcription) {
+          return reply.status(500).send({ error: 'Failed to transcribe audio' });
+        }
+  
+        if (!embeddings) {
+          return reply.status(500).send({ error: 'Failed to generate embeddings' });
+        }
+  
+  
+        const result = await db.insert(schema.audioChunks).values({
+          roomId,
+          transcription,
+          embeddings,
+        }).returning();
+  
+  
+        const chunk = result[0];
+  
+        if (!chunk) {
+          return reply.status(500).send({ error: 'Failed to save audio chunk' });
+        }
+  
+        return reply.status(201).send({ chunkId: chunk.id });
+      } catch (error) {
+        return reply.status(500).send({ error: 'Internal server error' });
+      }
+    }
+  )
 } 
